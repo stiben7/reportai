227c37ec2d747c0149df5565ce8187ea123c155b
stiben7 <stevenfernandezsrf@gmail.com>
2026-06-28 01:50:12 +0800

    Change export from default to module.exports

--- sheets.js
+++ sheets.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -32,4 +32,4 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ error: err.message || 'Internal server error' });
   }
-}
+};
